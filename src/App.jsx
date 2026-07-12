f717e97b7d59d921f96c29885c79374aa96824f1
mateo.dev <mateoortiz98a@gmail.com>
2026-07-12 18:22:33 -0300

    veri

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Routes, Route, Navigate } from 'react-router'
-import { LoginScreen } from './Screens/LoginScreen/LoginScreen.jsx'
-import { RegisterScreen } from './Screens/RegisterScreen/RegisterScreen.jsx'
-import { VerifyScreen } from './Screens/VerifyScreen/VerifyScreen.jsx' // 👈 Importamos la nueva pantalla
-import { HomeScreen } from './Screens/HomeScreen/HomeScreen.jsx'
-import { ProfileScreen } from './Screens/ProfileScreen/ProfileScreen.jsx'
-import { ForgotPasswordScreen } from './Screens/ForgotPasswordScreen/ForgotPasswordScreen.jsx'
-import { ResetPasswordScreen } from './Screens/ResetPasswordScreen/ResetPasswordScreen.jsx'
+import { LoginScreen } from './Screens/LoginScreen/LoginScreen'
+import { RegisterScreen } from './Screens/RegisterScreen/RegisterScreen'
+import { VerifyScreen } from './Screens/VerifyScreen/VerifyScreen' 
+import { HomeScreen } from './Screens/HomeScreen/HomeScreen'
+import { ProfileScreen } from './Screens/ProfileScreen/ProfileScreen'
+import { ForgotPasswordScreen } from './Screens/ForgotPasswordScreen/ForgotPasswordScreen'
+import { ResetPasswordScreen } from './Screens/ResetPasswordScreen/ResetPasswordScreen'
 
 function isAuthenticated() {
     return !!localStorage.getItem('access_token')
@@ -15,15 +15,23 @@ function isAuthenticated() {
 const App = () => {
     return (
         <Routes>
+            {/* Primero declaramos las rutas fijas y exactas de autenticación */}
             <Route path='/login' element={<LoginScreen />} />
             <Route path='/register' element={<RegisterScreen />} />
-            <Route path='/verify' element={<VerifyScreen />} /> {/* 👈 Agregamos la ruta pública */}
+            
+            {/* Blindamos la ruta de verificación */}
+            <Route path='/verify' element={<VerifyScreen />} /> 
+            
             <Route path='/home' element={<HomeScreen />} />
             <Route path='/profile' element={<ProfileScreen />} />
             <Route path='/forgot-password' element={<ForgotPasswordScreen />} />
             <Route path='/reset-password' element={<ResetPasswordScreen />} />
-            <Route path='/' element={isAuthenticated() ? <Navigate to='/home' /> : <Navigate to='/login' />} />
-            <Route path='/*' element={isAuthenticated() ? <Navigate to='/home' /> : <Navigate to='/login' />} />
+            
+            {/* Raíz del sitio */}
+            <Route path='/' element={isAuthenticated() ? <Navigate to='/home' replace /> : <Navigate to='/login' replace />} />
+            
+            {/* Captura de rutas no encontradas (404 / Comodín) al final de todo */}
+            <Route path='*' element={isAuthenticated() ? <Navigate to='/home' replace /> : <Navigate to='/login' replace />} />
         </Routes>
     )
 }
